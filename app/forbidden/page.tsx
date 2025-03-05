2b9304bd139b88b08d55d3f738c935af1cf97066
elale1984 <edward.gratefuldead1@gmail.com>
2025-03-05 16:53:32 -0600

    implement admin dashboard with user role management and search functionality

---
+++ app/forbidden/page.tsx
@@ -0,0 +1,10 @@
+// app/402/page.tsx
+export default function ForbiddenPage() {
+  return (
+    <div className="flex items-center justify-center ">
+      <h3 className="text-white text-4xl">402</h3>
+      <h1 className="text-red-600 text-6xl">|</h1>
+      <h3 className="text-white text-4xl">Forbidden</h3>
+    </div>
+  );
+}
